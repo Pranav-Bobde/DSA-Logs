101a3e353743e7b8fd079c72f552f098523addb6
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-06-15 00:05:09 +0530

    Create Remove-Linked-List-Elements.java

---
+++ Fast_&_Slow_Pointers/Day1/Remove-Linked-List-Elements.java
@@ -0,0 +1,21 @@
+//Link: https://leetcode.com/problems/remove-linked-list-elements/
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null || (head.next == null && head.val == val)) return null;
+        
+        ListNode temp = head;
+        
+        while(head!=null && head.val==val)
+            head = head.next;
+        if(head==null) return null;
+        while(temp!=null && temp.next!=null){
+            if(temp.next.val == val){
+                temp.next = temp.next.next;
+            }else
+                temp = temp.next;
+        }
+        if(head.next == null && head.val == val) return null;
+        return head;
+    }
+}
