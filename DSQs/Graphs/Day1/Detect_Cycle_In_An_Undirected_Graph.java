36d6d540e60e92d62fc9ed7d1d2e9624256fe65b
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-06-17 22:03:07 +0530

    Update Detect_Cycle_In_An_Undirected_Graph.java

--- DSQs/Graphs/Day1/Detect_Cycle_In_An_Undirected_Graph.java
+++ DSQs/Graphs/Day1/Detect_Cycle_In_An_Undirected_Graph.java
@@ -2,6 +2,7 @@
 
 //Time & Space: O(N) O(1) or O(V+E) O(V)
 
+//BFS
 class Node {
     int first;
     int second;
@@ -48,6 +49,36 @@ class Solution
                 if(checkForCycle(adj, i,vis)) 
                     return true;
     
+        return false;
+    }
+}
+
+
+//DFS
+class Solution {
+    public boolean checkForCycle(int node, int parent, boolean vis[], ArrayList<ArrayList<Integer>> adj) {
+        vis[node] = true;
+        for (Integer it: adj.get(node)) {
+            if (vis[it] == false) {
+                if (checkForCycle(it, node, vis, adj) == true)
+                    return true;
+            } else if (it != parent)
+                return true;
+        }
+
+        return false;
+    }
+    
+    public boolean isCycle(int V, ArrayList < ArrayList < Integer >> adj) {
+        boolean vis[] = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == false) {
+                if (checkForCycle(i, -1, vis, adj))
+                    return true;
+            }
+        }
+
         return false;
     }
 }
