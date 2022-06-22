dbd192a9d6671fefd9896b4efef6f987ce0047e4
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-06-22 06:04:17 +0530

    Update Topological_Sort.java

--- DSQs/Graphs/Day3/Topological_Sort.java
+++ DSQs/Graphs/Day3/Topological_Sort.java
@@ -1,7 +1,8 @@
 //Link: https://practice.geeksforgeeks.org/problems/topological-sort/1/
 
-//Time & Space Complexity: O(N) O(N)
+//Time & Space Complexity: O(N + E) O(N)
 
+//DFS
 class Solution
 {
     static void topo(int node, Stack<Integer> s, int[] vis, ArrayList<ArrayList<Integer>> adj){
@@ -27,6 +28,44 @@ class Solution
         for(int i=0; i<V; i++){
             ans[i] = s.pop();
         }
+        return ans;
+    }
+}
+
+//BFS (Kahn's Algorithm)
+
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        int[] ans = new int[V];
+        Stack<Integer> s = new Stack<>();
+        int[] indegree = new int[V];
+        
+        for(ArrayList<Integer> al : adj){
+            for(int i: al){
+                indegree[i]++;
+            }
+        }
+        
+        for(int i=0; i<V; i++)
+            if(indegree[i] == 0)
+                s.push(i);
+        
+        int idx = 0;
+        while(!s.isEmpty()){
+            int node = s.pop();
+            ans[idx++] = node;
+            
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0)
+                    s.push(it);
+            }
+            
+        }
+        
         return ans;
     }
 }
