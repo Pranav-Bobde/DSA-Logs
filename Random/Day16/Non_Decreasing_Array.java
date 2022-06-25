ecf886862c1210f7d8cdefeb306b85af5b8d1f84
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-06-25 15:46:50 +0530

    Create Non_Decreasing_Array.java

---
+++ Random/Day16/Non_Decreasing_Array.java
@@ -0,0 +1,22 @@
+//Link: https://leetcode.com/problems/non-decreasing-array/
+
+//Time & Space Complexity: O(N) O(1)
+
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length;
+        boolean changed = false;
+        for(int i=0; i<n-1; i++){
+            if(nums[i] <= nums[i+1])
+                continue;
+            if (changed)
+                return false;
+            if( i==0 || nums[i+1] >= nums[i-1])
+                nums[i] = nums[i+1];
+            else
+                nums[i+1] = nums[i];
+            changed = true;
+        }
+        return true;
+    }
+}
