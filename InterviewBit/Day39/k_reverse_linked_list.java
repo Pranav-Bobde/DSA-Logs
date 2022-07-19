334538b6bdcc62b714cc8969d06163dea1504a93
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-07-20 00:27:32 +0530

    Create k_reverse_linked_list.java

---
+++ InterviewBit/Day39/k_reverse_linked_list.java
@@ -0,0 +1,34 @@
+//Link: https://www.interviewbit.com/problems/k-reverse-linked-list/
+
+//Time & Space Complexity: O(N) O(1)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode A, int B) {
+        ListNode current = A;
+        ListNode next = null;
+        ListNode prev = null;
+        
+        int count = 0;
+
+        while (count < B && current != null) {
+           next = current.next;
+           current.next = prev;
+           prev = current;
+           current = next;
+           count++;
+        }
+
+        if (next != null) 
+          A.next = reverseList(next, B);
+
+        return prev;
+    }
+}
