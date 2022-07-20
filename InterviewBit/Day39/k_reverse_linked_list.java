f7d789af0f8d2f386be3871faa2a4613f827ebf8
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-07-20 21:51:56 +0530

    Update k_reverse_linked_list.java

--- InterviewBit/Day39/k_reverse_linked_list.java
+++ InterviewBit/Day39/k_reverse_linked_list.java
@@ -11,6 +11,47 @@
  * }
  */
 public class Solution {
+    ListNode getKth(ListNode curr, int k){
+        while(curr!=null && k-->0){
+            curr = curr.next;
+        }
+        return curr;
+    }
+    public ListNode reverseList(ListNode head, int k) {
+        if(head==null || head.next == null) return head;
+        ListNode dummy = new ListNode(0), grpPrev = dummy;
+        dummy.next = head;
+        while(true){
+            ListNode kth = getKth(grpPrev, k);
+            if(kth == null)
+                break;
+            ListNode grpNext = kth.next;
+            ListNode curr = grpPrev.next, prev = grpNext;
+            while(curr!=grpNext){
+                ListNode nxt = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nxt;
+            }
+            ListNode temp = grpPrev.next;
+            grpPrev.next = kth;
+            grpPrev = temp;
+        }
+        return dummy.next;
+    }
+}
+
+
+//Time & Space Complexity: O(N) O(N)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
     public ListNode reverseList(ListNode A, int B) {
         ListNode current = A;
         ListNode next = null;
