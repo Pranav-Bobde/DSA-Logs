afdddf8d66d5571d455a57e386238772bd3f1e2d
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-07-26 00:41:51 +0530

    Update binary_tree_maximum_path_sum.java

--- InterviewBit/Day44/binary_tree_maximum_path_sum.java
+++ InterviewBit/Day44/binary_tree_maximum_path_sum.java
@@ -4,15 +4,16 @@
 
 class Solution {
     int dfs(TreeNode root, int[] res){
-        if(root==null) return -3001;
+        if(root==null) return 0;
         int left = dfs(root.left, res);
         int right = dfs(root.right, res);
         int temp = Math.max( Math.max(left, right) + root.val, root.val );
-        res[0] = Math.max(res[0], (left<0?0:left)+(right<0?0:right)+root.val);
+        int ans = Math.max(temp, left+right+root.val);
+        res[0] = Math.max(res[0], ans);
         return temp;
     }
     public int maxPathSum(TreeNode root) {
-        int[] res = new int[]{-3001};
+        int[] res = new int[]{Integer.MIN_VALUE};
         dfs(root, res);
         return res[0];
     }
