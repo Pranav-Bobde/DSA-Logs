1ad7ea22553cc5770b8d346b6a0b250cfb67c60e
Pranav <66965591+Pranav-Bobde@users.noreply.github.com>
2022-09-13 17:30:57 +0530

    Create kth-largest-element-in-an-array.java

---
+++ DSA-Sheet/Day15/kth-largest-element-in-an-array.java
@@ -0,0 +1,16 @@
+//Link: https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+//Time & Space: O(NlogK) O(K)
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : nums){
+            pq.add(i);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        return pq.poll();
+    }
+}
